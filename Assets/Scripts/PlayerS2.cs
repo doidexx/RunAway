7ccb234a8093da1948fda5df042b0d056f7c8641
doidexx <doidexx000@gmail.com>
2018-12-09 22:48:22 -0500

    final Demo

--- Assets/Scripts/PlayerS2.cs
+++ Assets/Scripts/PlayerS2.cs
@@ -194,9 +194,9 @@ public class PlayerS2 : MonoBehaviour {
         if ((other.gameObject.tag == "table" || other.gameObject.tag == "locker") && !bumpT) {
             bumpT = true;
         }
-        if (other.gameObject.tag == "garbage") {
+        if (other.gameObject.tag == "garbage" &&  transform.position.z < other.transform.position.z) {
             bumpT = true;
-            other.rigidbody.AddForce(Vector3.forward * 5, ForceMode.Impulse);
+            other.rigidbody.AddForce(Vector3.forward * 10, ForceMode.Impulse);
         }
         if (other.gameObject.tag == "car" && transform.position.z < other.transform.position.z - 4) {
             playerAni.SetInteger("state", 9);
